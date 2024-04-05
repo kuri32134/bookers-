362b3549915d3e72f71ff06b0b2f33dc51daace3
kuri32134 <161860576+kuri32134@users.noreply.github.com>
2024-04-05 06:51:47 +0000

    [add]edit,destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,8 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.new
+    @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -17,8 +18,15 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
   private
 
   def book_params
